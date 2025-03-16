ccc7e4c811593b09116b4b5e60787dfc743dc6dd
Daniel Souza <58640987+Danielgsouza@users.noreply.github.com>
2025-03-16 15:32:49 -0300

    Alteando controle de acesso

--- acesso/access-control.js
+++ acesso/access-control.js
@@ -4,11 +4,6 @@ document.addEventListener("DOMContentLoaded", function() {
   inputCpf.focus();
 });
 
-// inputCpf.addEventListener("input", function() {
-//   if(inputCpf.value.length == 11){
-//     handleSubmit()
-//   }
-// });
 
 inputCpf.addEventListener("focusout", function() {
   setTimeout(() => {
@@ -24,7 +19,6 @@ var notyf = new Notyf({
 });
 
 document.getElementById("access-control-form").addEventListener("submit", async function (event) {
-  event.preventDefault(); // Impede o recarregamento da página
   var form = new FormData(document.getElementById("access-control-form"));
   console.log(form)
   try {
@@ -33,7 +27,7 @@ document.getElementById("access-control-form").addEventListener("submit", async 
       body: form,
     });
     const dados = await response.json();
-    
+   
     if (dados.data.length === 0) {
         // Display an error notification
         notyf.error('Usuário não econtrado e/ou não está cadastrado!');
@@ -41,65 +35,68 @@ document.getElementById("access-control-form").addEventListener("submit", async 
         return;
     }
       
-    const socio = dados.data[dados.data.length - 1]; // Última posição do array
-    document.getElementById('nome').textContent = socio.nome;
-    document.getElementById('cpf').textContent = socio.cpf;
-    document.getElementById('status').innerHTML = dados.status ? '<span class="text-success"><i class="fas fa-check text-success"></i> Ativo</span>' : '<span class="text-danger"><i class="fas fa-xmark text-danger"></i> Pendente</span>';
+      const socio = dados.data[dados.data.length - 1]; // Última posição do array
+      document.getElementById('nome').textContent = socio.nome;
+      document.getElementById('cpf').textContent = socio.cpf;
+      document.getElementById('status').innerHTML = dados.status ? '<span class="text-success"><i class="fas fa-check text-success"></i> Ativo</span>' : '<span class="text-danger"><i class="fas fa-xmark text-danger"></i> Pendente</span>';
 
-    // document.getElementById('status-icon').innerHTML = dados.status ? '<span class="text-success m-auto" style="font-size: 5rem;"><i class="fas fa-check text-success"></i> </span>' : '<span class="text-danger"><i class="fas fa-xmark text-danger" style="font-size: 5rem;"></i></span>';
+      document.getElementById('status-icon').innerHTML = dados.status ? '<span class="text-success m-auto" style="font-size: 5rem;"><i class="fas fa-check text-success"></i> </span>' : '<span class="text-danger"><i class="fas fa-xmark text-danger" style="font-size: 5rem;"></i></span>';
 
-    // Tentativa de carregar a imagem com diferentes extensões
-    const imagePathBase = `../users/uploads/${socio.cpf}`;
-    const imageExtensions = ['.png', '.jpg', '.jpeg']; // Extensões possíveis
-    let imagePath = null;
 
-    // Função para verificar se a imagem existe
-    function checkImage(extension) {
-      return new Promise((resolve, reject) => {
-        const img = new Image();
-        img.src = `${imagePathBase}${extension}`;
-        img.onload = () => resolve(`${imagePathBase}${extension}`); // Retorna o caminho da imagem se carregada com sucesso
-        img.onerror = () => reject(); // Rejeita se ocorrer um erro ao carregar a imagem
-      });
-    }
 
-    // Tentando carregar a imagem com as extensões possíveis
-    (async function loadImage() {
-      try {
-        for (let ext of imageExtensions) {
-          try {
-            imagePath = await checkImage(ext); // Tenta carregar a imagem
-            console.log(imagePath);
-            $('#foto-preview').attr('src', imagePath); // Se carregada, exibe a imagem
-            return; // Sai da função se a imagem for encontrada
-          } catch (error) {
-            // Caso a imagem não seja encontrada, tenta a próxima extensão
-            continue;
-          }
-        }
-        // Se nenhuma das imagens for encontrada, exibe a imagem padrão
-        $('#foto-preview').attr('src', '../assets/images/avatar/icon.png');
-      } catch (error) {
-        // Caso nenhum arquivo seja encontrado, exibe a imagem padrão
-        $('#foto-preview').attr('src', '../assets/images/avatar/icon.png');
+      const imagePathBase = `https://adminccm.com/users/uploads/${socio.cpf}`;
+      // console.log(imagePathBase);
+      const imageExtensions = ['.png', '.jpg', '.jpeg']; // Extensões possíveis
+      let imagePath = null;
+  
+      // Função para verificar se a imagem existe
+      function checkImage(extension) {
+        return new Promise((resolve, reject) => {
+          const img = new Image();
+          img.src = `${imagePathBase}${extension}`;
+          img.onload = () => resolve(`${imagePathBase}${extension}`); // Retorna o caminho da imagem se carregada com sucesso
+          img.onerror = () => reject(); // Rejeita se ocorrer um erro ao carregar a imagem
+        });
       }
-    })();
+  
+      // Tentando carregar a imagem com as extensões possíveis
+      (async function loadImage() {
+        try {
+          for (let ext of imageExtensions) {
+            try {
+              imagePath = await checkImage(ext); // Tenta carregar a imagem
+              console.log(imagePath);
+              $('#foto-preview').attr('src', imagePath); // Se carregada, exibe a imagem
+              return; // Sai da função se a imagem for encontrada
+            } catch (error) {
+              // Caso a imagem não seja encontrada, tenta a próxima extensão
+              continue;
+            }
+          }
+          // Se nenhuma das imagens for encontrada, exibe a imagem padrão
+          $('#foto-preview').attr('src', '../assets/images/avatar/icon.png');
+        } catch (error) {
+          // Caso nenhum arquivo seja encontrado, exibe a imagem padrão
+          $('#foto-preview').attr('src', '../assets/images/avatar/icon.png');
+        }
+      })();
 
-    document.getElementById('foto-preview').onerror = function() {
-      this.src = '../assets/images/avatar/icon.png';
-    }
-    inputCpf.value = ''; // Limpar o campo de pesquisa
-    inputCpf.focus(); // Focar no campo de pesquisa após limpar o campo
+      document.getElementById('foto-preview').onerror = function() {
+        this.src = '../assets/images/avatar/icon.png';
+      }
 
-    // Limpar os dados após 5 segundos
-    setTimeout(() => {
-      document.getElementById('nome').textContent = '';
-      document.getElementById('cpf').textContent = '';
-      document.getElementById('status').textContent = '';
-      // document.getElementById('status-icon').textContent = '';
-      document.getElementById('foto-preview').src = '../assets/images/avatar/icon.png';
+      inputCpf.value = ''; // Limpar o campo de pesquisa
+      inputCpf.focus(); // Focar no campo de pesquisa após limpar o campo
 
-    }, 3000);
+      // Limpar os dados após 5 segundos
+      setTimeout(() => {
+        document.getElementById('nome').textContent = '';
+        document.getElementById('cpf').textContent = '';
+        document.getElementById('status').textContent = '';
+        document.getElementById('status-icon').textContent = '';
+        document.getElementById('foto-preview').src = '../assets/images/avatar/icon.png';
+
+      }, 3000);
     // }else{
     //   // Display an error notification
     //   notyf.error('Usuário não econtrado e/ou não está cadastrado!');
