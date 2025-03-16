5ab8fa84ea402ec601378442ae6012ceb165fca8
Daniel Souza <58640987+Danielgsouza@users.noreply.github.com>
2025-03-16 15:35:59 -0300

    Update access-control.js

--- acesso/access-control.js
+++ acesso/access-control.js
@@ -4,7 +4,6 @@ document.addEventListener("DOMContentLoaded", function() {
   inputCpf.focus();
 });
 
-
 inputCpf.addEventListener("focusout", function() {
   setTimeout(() => {
     inputCpf.focus();
