9e4c62563d211a2e4ec1c95c882911bec78908b5
Daniel Souza <58640987+Danielgsouza@users.noreply.github.com>
2025-03-18 21:27:53 -0300

    Incluindo o dateDiff  para verificar se passaram mais de 60 dias

--- acesso/queries/sql_get_access_control.php
+++ acesso/queries/sql_get_access_control.php
@@ -7,8 +7,12 @@ $ArrayDados = filter_input_array(INPUT_POST, FILTER_DEFAULT);
 $cpf = $ArrayDados['socio-id'];
 
 try {
-    // Preparar a query para pegar o último pagamento
-    $sql = "SELECT * FROM pagamentos WHERE cpf = :cpf and delet = ''  ORDER BY ANO DESC, MES DESC LIMIT 1 ";
+    // Preparar a query para pegar o último pagamento e calcular a diferença em dias
+    $sql = "SELECT *, DATEDIFF(CURDATE(), DATE_FORMAT(CONCAT(ano, '-', mes, '-01'), '%Y-%m-%d')) AS dias_desde_pagamento
+            FROM pagamentos
+            WHERE cpf = :cpf AND delet = '' 
+            ORDER BY ANO DESC, MES DESC LIMIT 1";
+    
     $stmt = $pdo->prepare($sql);
     $stmt->bindParam(':cpf', $cpf, PDO::PARAM_STR);
     
@@ -19,21 +23,12 @@ try {
     if ($stmt->rowCount() > 0) {
         // Obtém os dados como um array associativo
         $pagamento = $stmt->fetch(PDO::FETCH_ASSOC);
-      
-        // Verifica a data do último pagamento (ano e mês)
-        $mes_pagamento = $pagamento['mes'];
-        $ano_pagamento = $pagamento['ano']; // Ano do pagamento
-        $data_pagamento = DateTime::createFromFormat('Y-m', "$ano_pagamento-$mes_pagamento");
+        
+        // A diferença de dias desde o pagamento
+        $dias_desde_pagamento = $pagamento['dias_desde_pagamento'];
 
-        // Data atual
-        $data_atual = new DateTime();
-
-        // Adiciona 60 dias à data do pagamento para comparar com a data atual
-        $data_limite = clone $data_pagamento; // Clona a data de pagamento para não modificar a original
-        $data_limite->add(new DateInterval('P60D')); // Adiciona 60 dias à data do pagamento
-
-        // Comparar se a data atual é superior a 60 dias após o pagamento
-        if ($data_atual > $data_limite) {
+        // Verifica se passaram mais de 60 dias
+        if ($dias_desde_pagamento > 60) {
             // Prazo superior a 60 dias
             echo json_encode([
                 'status' => false,
